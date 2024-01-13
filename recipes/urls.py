88f10424351d369c6d0aa96e670598e416f5a8e6
Gelson Lopes <pmikia17@gmail.com>
2024-01-13 05:35:58 -0100

    Todas as alteracoes antes do deploy.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+app_name = 'recipes'
+
 
 urlpatterns = [
-    path('', home),
+    path('', views.RecipeListViewHome.as_view(), name="home"),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(), name="search"
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(), name="category"
+    ),
+    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+
 ]
